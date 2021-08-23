f71df1490b4ace63447e7d6d284ff3ed3bf74357
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-23 21:04:06 +0800

    [Frontend][GG] refactor: update app.js file, imported browser router for react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import './App.css';
+import logo from "./logo.png";
 import Header from './features/components/Header/Header';
 import Landing from './features/components/Landing/Landing';
 import Footer from "./features/components/Footer/Footer";
+import { Route, Link, BrowserRouter, Switch } from "react-router-dom";
+import {Container, Nav, Navbar} from "react-bootstrap";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faUserCircle} from "@fortawesome/free-solid-svg-icons";
 import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Landing/>
-      <Footer/>
+      <BrowserRouter>
+          <Navbar bg="white">
+              <Container>
+                  <Navbar.Brand href="/">
+                      <img
+                          src={logo}
+                          width="160px"
+                          className="d-inline-block align-top"
+                          alt="React Bootstrap logo"
+                      />
+                  </Navbar.Brand>
+                  <Nav>
+                      <Nav.Link>
+                          <b>Sign up</b>&nbsp;
+                          <FontAwesomeIcon icon={faUserCircle}/>
+
+                      </Nav.Link>
+                  </Nav>
+              </Container>
+          </Navbar>
+
+
+          <Switch>
+              <Route exact path="/" component={Landing}></Route>
+              <Route exact path="/link1" component={Landing}></Route>
+              <Route exact path="/link2" component={Landing}></Route>
+              {/* <Route path="*" component={NotFoundPage}></Route> */}
+          </Switch>
+
+      </BrowserRouter>
+
+      
+
+      
     </div>
   );
 }
--- src/features/components/Landing/Landing.js
+++ src/features/components/Landing/Landing.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Footer from "../Footer/Footer";
 
 function Landing() {
     return (
         <div>
             Landing
+
+
+            <Footer/>
         </div>
     )
 }
