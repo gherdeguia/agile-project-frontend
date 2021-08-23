33cc290c9be1337ee5eb4474aed9352e8e68bfb2
CANUTLO <lorenz.alvin.canuto@oocl.com>
2021-08-23 19:25:08 +0800

    [Frontend][Lorenz] refactor: change footer

--- src/features/components/Footer/Footer.js
+++ src/features/components/Footer/Footer.js
@@ -5,44 +5,49 @@ import './Footer.css'
 
 function Footer() {
     return (
-        <Navbar bg="dark"  fixed="bottom">
+        <Navbar bg="dark" fixed="bottom">
             <Container>
                 <Navbar.Brand href="/">
                     <Table>
                         <tr>
                             <td>
-                                About
+                                <a className="nav-link" href="/">About</a>
                             </td>
                             <td>
-                                Popular Movies
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Contact Us
-                            </td>
-                            <td>
-                                Upcoming Movies
+                                <a className="nav-link" href="/">Popular Movies</a>
                             </td>
                         </tr>
                         <tr>
                             <td>
+                                <a className="nav-link" href="/">Contact Us</a>
                             </td>
                             <td>
-                                Cinemas
+                                <a className="nav-link" href="/">Upcoming Movies</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                            </td>
+                            <td>
+                                <a className="nav-link" href="/">Cinemas</a>
                             </td>
                         </tr>
                     </Table>
-                    <Nav.Item></Nav.Item>
                 </Navbar.Brand>
                 <Nav>
-                    <img
-                        src={logo}
-                        width="160px"
-                        className="d-inline-block align-top"
-                        alt="React Bootstrap logo"
-                    />
-                    <span>© 2021 | CTRL ALT ELITE</span>
+                    <div>
+                        <div>
+                            <img
+                                src={logo}
+                                width="160px"
+                                className="d-inline-block align-top"
+                                alt="React Bootstrap logo"
+                            />
+                        </div>
+                        <div>
+                            <span>© 2021 | CTRL ALT ELITE</span>
+                        </div>
+                    </div>
                 </Nav>
             </Container>
         </Navbar>
