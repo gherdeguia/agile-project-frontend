6959863a82ef0f9aec7aee5ff7cf44ddf63970b1
CANUTLO <lorenz.alvin.canuto@oocl.com>
2021-08-23 19:41:45 +0800

    [Frontend][Lorenz] feat: added SearchBar in Landing page

--- src/features/components/Landing/Landing.js
+++ src/features/components/Landing/Landing.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Trending from "./Trending";
+import SearchBar from "./SearchBar";
 
 function Landing() {
     return (
         <div>
-            Landing
+            <Trending/>
+            <SearchBar/>
         </div>
     )
 }
---
+++ src/features/components/Landing/Trending.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Trending() {
+    return (
+        <div>
+            <h4><b>Trending</b> Movies</h4>
+        </div>
+    )
+}
+
+export default Trending
