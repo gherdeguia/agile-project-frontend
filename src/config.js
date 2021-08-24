b0ba197f9988c5499a739d97a2662129c7edc79c
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-24 15:24:30 +0800

    [Frontend][GG] refactor : update config.js file & cinemas api

---
+++ src/config.js
@@ -0,0 +1,27 @@
+const prod = {
+ url: {
+ API_URL: "https://cine-q-prod.herokuapp.com/",
+ },
+};
+
+const staging = {
+ url: {
+ API_URL: "https://cine-q.herokuapp.com/",
+ },
+};
+
+const dev = {
+ url: {
+ API_URL: "http://localhost:8080",
+ },
+};
+
+let config = dev;
+if(process.env.NODE_ENV === 'staging') {
+    config = staging
+}
+else if(process.env.NODE_ENV === 'production'){
+    config = prod
+};
+
+export default config;
--- src/features/apis/cinemas.js
+++ src/features/apis/cinemas.js
@@ -1,5 +1,7 @@
+
 import api from "./api"
 
 export const getCinemas = () => {
+    console.log(process.env);
     return api.get("/cinemas");
 }
