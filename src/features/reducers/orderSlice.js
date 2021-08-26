bc6c3fcc5a8e5f0c68d6c41bf3e9c6f5f6d35467
omagara <omagara@oocl.com>
2021-08-26 11:20:52 +0800

    [Frontend][Ramon]feat: added screeningSlice; modified order slice

--- src/features/reducers/orderSlice.js
+++ src/features/reducers/orderSlice.js
@@ -5,18 +5,22 @@ const orderSlice = createSlice({
     initialState: {
         cinema:null,
         movie:null,
-        ScreeningTime:null,
-        Seats:null
+        screeningTime:null,
+        seats:null
     },
     reducers: {
         SelectMovieAndCinema(state, action) {
             state.cinema = action.payload.cinema
             state.movie = action.payload.movie
+        },
+        SelectScreeningTime(state, action) {
+            state.screeningTime = action.payload
         }
+
     }
 })
 
-export const {SelectMovieAndCinema} = orderSlice.actions;
+export const {SelectMovieAndCinema, SelectScreeningTime} = orderSlice.actions;
 
 export const getOrder = (state) => state.order
 
