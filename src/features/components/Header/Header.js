5727aa763503948cb847a44546ef0bfaf44eb47e
omagara <omagara@oocl.com>
2021-08-26 22:42:55 +0800

    [Frontend][Ramon]refactor: reformatted Footer styles

--- src/features/components/Header/Header.js
+++ src/features/components/Header/Header.js
@@ -1,6 +1,6 @@
-import logo from './logo.png';
+import logo from './CINEQ-2.png';
 import React from 'react'
-import {Container, Nav, Navbar} from "react-bootstrap";
+import {Container, Nav, Navbar, Table} from "react-bootstrap";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faUserCircle} from "@fortawesome/free-solid-svg-icons";
 
@@ -9,21 +9,30 @@ function Header() {
     return (
         <Navbar bg="white">
             <Container>
+                <Table>
+                    <tr>
+                        <td>
                 <Navbar.Brand href="/">
+                    <div style={{marginRight:"300px"}}>
                     <img
                         src={logo}
-                        width="160px"
+                        width="170px"
                         className="d-inline-block align-top"
                         alt="React Bootstrap logo"
                     />
+                    </div>               
                 </Navbar.Brand>
+                </td>
+                <td>
                 <Nav>
                     <Nav.Link>
                         <b>Sign</b> up
                         <FontAwesomeIcon icon={faUserCircle}/>
-
                     </Nav.Link>
                 </Nav>
+                </td>
+                </tr>
+                </Table>
             </Container>
         </Navbar>
     )
